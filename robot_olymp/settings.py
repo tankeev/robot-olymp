9e80a0a1c52d0d37c3b9feff434156fa29f1d3da
unknown <tankeev@gmail.com>
2017-12-27 05:27:31 -0800

    adda

--- robot_olymp/settings.py
+++ robot_olymp/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['robot-olymp.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['https://robot-olymp.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
