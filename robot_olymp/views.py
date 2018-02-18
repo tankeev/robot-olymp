3b08747e72264014ff7fd6b85c7497b5a66d148b
unknown <tankeev@gmail.com>
2018-02-17 22:43:07 -0800

    pdf categories added

--- robot_olymp/views.py
+++ robot_olymp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -24,3 +25,24 @@ def auth_password_change_done(request):
 def password_reset_complete(request):
     return render(request, 'registration/password_reset_complete.html')
 
+def sumo(request):
+    image_data = open("static/pdf/1-LEGO_SUMO.pdf", "rb").read()
+    return HttpResponse(image_data, content_type="application/pdf")
+
+def lego_line_follower(request):
+    image_data = open("static/pdf/2-LEGO_LINEFOLLOWER.pdf", "rb").read()
+    return HttpResponse(image_data, content_type="application/pdf")
+
+
+def arduino_line_follower(request):
+    image_data = open("static/pdf/3-ARDUINO_LINEFOLLOWER.pdf", "rb").read()
+    return HttpResponse(image_data, content_type="application/pdf")
+
+
+def ollo_race(request):
+    image_data = open("static/pdf/4-OLLO_RACE.pdf", "rb").read()
+    return HttpResponse(image_data, content_type="application/pdf")
+
+def ollo_ball(request):
+    image_data = open("static/pdf/5-OLLO_BALL.pdf", "rb").read()
+    return HttpResponse(image_data, content_type="application/pdf")
