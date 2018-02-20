106db2b0c7990abfcc19c812fa45eeb8dbd6f581
unknown <tankeev@gmail.com>
2018-02-20 02:35:11 -0800

    dns pointing to herokuapp

--- robot_olymp/settings.py
+++ robot_olymp/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['robot-olymp.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['robot-olymp.herokuapp.com','127.0.0.1','robotofest.org']
 
 
 # Application definition
